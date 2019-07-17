96f6d402eba3c9abe0d4385cf9c601e0a5f50018
Eduardo Ramme <eduramme@gmail.com>
2019-07-17 05:28:58 -0300

    update sidebar & create register page

--- src/components/Notifications.js
+++ src/components/Notifications.js
@@ -1,5 +1,36 @@
 import React from "react";
 
-const Notifications = () => <h1>Notifications</h1>;
+const Notifications = () => (
+  <div className="container">
+    <h1>Notifications</h1>
+    <table>
+      <tr>
+        <th>Firstname</th>
+        <th>Lastname</th>
+        <th>Savings</th>
+      </tr>
+      <tr>
+        <td>Peter</td>
+        <td>Griffin</td>
+        <td>$100</td>
+      </tr>
+      <tr>
+        <td>Lois</td>
+        <td>Griffin</td>
+        <td>$150</td>
+      </tr>
+      <tr>
+        <td>Joe</td>
+        <td>Swanson</td>
+        <td>$300</td>
+      </tr>
+      <tr>
+        <td>Cleveland</td>
+        <td>Brown</td>
+        <td>$250</td>
+      </tr>
+    </table>
+  </div>
+);
 
 export default Notifications;
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,5 +1,85 @@
 import React from "react";
+import styled from "styled-components";
 
-const Register = () => <h1>Register new user</h1>;
+import { Form, Button, Jumbotron, Col } from "react-bootstrap";
+
+const Formbox = styled.form`
+  margin: 100px 0;
+  background: #fff;
+  width: 400px;
+  padding: 40px;
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.3);
+`;
+
+const Inputfield = styled.input`
+  outline: none;
+  display: block;
+  width: 100%;
+  margin: 0 0 20px;
+  padding: 10px 15px;
+  border: 1px solid #ccc;
+  box-sizing: border-box;
+  font-size: 14px;
+  font-weight: 400;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  transition: 0.2s linear;
+  &input:focus {
+    color: #333;
+    border: 1px solid #44c4e7;
+  }
+`;
+
+const Inputbutton = styled.input`
+  cursor: pointer;
+  background: #44c4e7;
+  width: 100%;
+  padding: 10px 15px;
+  border: 0;
+  color: #fff;
+  font-size: 14px;
+  font-weight: 400;
+  &:hover {
+    background: #369cb8;
+  }
+`;
+
+const Tittle = styled.h2`
+  margin: 0 0 40px;
+  line-height: 1;
+  color: #44c4e7;
+  font-size: 25px;
+  font-weight: 400;
+`;
+
+const Flexbox = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const Register = () => (
+  <div>
+    <Flexbox>
+      <Formbox>
+        <Tittle>Register</Tittle>
+        <Inputfield
+          name="email"
+          type="text"
+          placeholder="email"
+          //   value={this.state.userName}
+          //   onChange={this.handleInputChange}
+        />
+        <Inputfield
+          name="name"
+          type="text"
+          placeholder="name"
+          //   value={this.state.password}
+          //   onChange={this.handleInputChange}
+        />
+        <Inputbutton type="submit" value="Submit" />
+      </Formbox>
+    </Flexbox>
+  </div>
+);
 
 export default Register;
