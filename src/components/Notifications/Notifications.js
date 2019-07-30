5ce0e394471a6be3200f01ee650ba171888f4934
Rafa Cavalcante <rafa.pgcavalcante@gmail.com>
2019-07-30 15:29:25 -0300

    add: notifications components

--- src/components/Notifications/Notifications.js
+++ src/components/Notifications/Notifications.js
@@ -1,76 +1,134 @@
 import React, { Component } from "react";
 import styled from "styled-components";
-// import notifications from "../../JSON/notifications.json";
-import Row from "./Row";
+import axios from 'axios';
 
-// Objetivo de importar notifications e da função render notifications é simular
-// uma request padrao para a API
-
-const SearchBar = styled.input`
-  display: block;
-  margin-top: 40px;
-  margin-left: 10%;
-  border: 3px solid #00b4cc;
-  padding: 5px;
-  height: 20px;
-  border-radius: 5px;
-  outline: none;
-  color: black;
+const Main = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 60px;
 `;
+
+const Table = styled.table`
+  /* text-align: left; */
+  font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+  border-collapse: collapse;
+  border: 3px solid #ddd;
+  width: 100%;
+  tbody:before {
+    content: "-";
+    display: block;
+    line-height: 0.6em;
+    color: transparent;
+  }
+`;
+
+const Th = styled.th`
+  border: 1px solid black;
+  padding-top: 12px;
+  padding-bottom: 12px;
+  text-align: left;
+  background-color: #6bd2c9;
+  font-size: 18px;
+  color: white;
+  font-weight: 700;
+  opacity: 0.65;
+`;
+
+const Tr = styled.tr`
+  border-bottom: 2px solid black;
+`;
+
+const Td = styled.td`
+  border-bottom: 2px solid #ddd;
+  &:nth-child(even) {
+    background-color: #f2f2f2;
+  }
+  &:hover {
+    background-color: #ddd;
+  }
+`;
+
 
 class Notifications extends Component {
   state = {
-    searchString: ""
-  };
+    searchString: "",
+    notifications:[],
+  }
 
-  // renderNotifications = searchString => {
-  //   console.log("render notifications no comando!");
-  //   const search = this.state.searchString;
-  //   const notificationsJson = notifications.notifications;
+  componentDidMount(){
+    this.getNotifications()
+}
 
-  //   if (search === "") {
-  //     return notificationsJson.map((not, i) => (
-  //       <Row notification={not} key={i} id={i} />
-  //     ));
-  //   } else {
-  //     var result = notificationsJson.filter(c =>
-  //       c.name.toLowerCase().includes(search.toLowerCase())
-  //     );
-
-  //     return result.map((not, i) => <Row notification={not} key={i} id={i} />);
-  //   }
-  // };
-
-  changeSearch = e =>
+getNotifications = () => {
+  fetch("http://localhost:8080/events", {
+  method: "GET",
+  credentials: "same-origin", // include, *same-origin, omit
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: "Basic YWRtaW46YWRtaW4="
+  },
+  redirect: "follow", // manual, *follow, error
+  referrer: "no-referrer" // no-referrer, *client
+})
+  .then(response => {
+    console.log("hello");
+    return response.json();
+  })
+  .then(data => {
+    console.log(data);
     this.setState({
-      searchString: e.target.value
+      notifications: data
     });
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+}
+
+
+  renderTableHeader() {
+    if (this.state.notifications[0]) {
+      let header = Object.keys(this.state.notifications[0]);
+      return header.map((key, index) => {
+        return <Th key={index}>{key.toUpperCase()}</Th>;
+      });
+    } else {
+      return null;
+    }
+  }
+
+
 
   render() {
+    const { notifications } = this.state;
+
     return (
-      <div className="container">
-        <h1>Notifications</h1>
-        <SearchBar
-          type="text "
-          placeholder="pesquisa por nome"
-          value={this.state.searchString}
-          onChange={this.changeSearch}
-        />
-        <table>
-          <thead>
-            <tr>
-              <th>nome</th>
-              <th>email</th>
-              <th>type</th>
-              <th>hora</th>
-              <th>data</th>
-            </tr>
-          </thead>
-          {/* <tbody>{this.renderNotifications()}</tbody> */}
-        </table>
-      </div>
+     <React.Fragment>
+       <Main>
+        
+        <Table>
+          <tbody>
+            <Tr>
+              {this.renderTableHeader()}
+            </Tr>
+            {notifications.map((notifications, index) => (
+              <tr key={notifications.id} >
+                <Td>{notifications.id}</Td>
+                <Td>{notifications.name}</Td>
+                <Td>{notifications.email}</Td>
+                <Td>{notifications.qtLeads}</Td>
+                <Td>{notifications.dtEnvio}</Td>
+                <Td>{notifications.hrEnvio}</Td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </Main>
+    </React.Fragment>
+      
+
     );
   }
 }
-
 export default Notifications;
