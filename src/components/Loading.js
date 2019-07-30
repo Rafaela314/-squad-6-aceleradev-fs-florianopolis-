e132d0c2a7c4722fd0b340dbe3c2e7d967a2ccfe
Rafa Cavalcante <rafa.pgcavalcante@gmail.com>
2019-07-30 19:25:27 -0300

    loading is ok

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,9 +1,7 @@
 import React from "react";
-// import ReactLoading from "react-loading";
+import ReactLoading from "react-loading";
 
 const Loading = ({ type, color }) => (
-  // <ReactLoading type={'bars'} color={'#6bd2c9'} height={667} width={375} />);
-  <h2>hello</h2>
-);
+  <ReactLoading type={'bars'} color={'#6bd2c9'} height={667} width={375} />);
 
 export default Loading;
